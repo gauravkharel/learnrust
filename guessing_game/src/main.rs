3335f43f7cc8f8eed7bfac687ba0ec169f9e8b52
gauravkharel <kharel619@gmail.com>
2024-05-13 18:49:56 +0545

    Final guessing game

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,15 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 fn main() {
     println!("Guess the number: ");
-    println!("Please input your guess");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    
+    print!("The secret number is : {secret_number} ");
+
+    loop{
+        println!("Please input your guess");
 
     let mut guess  = String::new();
 
@@ -9,5 +17,20 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
     
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
     println!("You guessed: {guess}");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => print!("Too small"),
+        Ordering::Greater => print!("Too big"),
+        Ordering::Equal => {
+            print!("You win!");
+            break;
+        }
+    }
+    }
 }
